a696c6122636a3d3833b00ce99fbcd463b7b36d2
Paul <meleeman01@live.com>
2018-09-28 02:40:59 -0500

    created a new app
    created a new app for testing views

---
+++ views/resources/views/posts/index.blade.php
@@ -0,0 +1,12 @@
+@extends ('layout')
+
+
+@section('content')
+	asefa;sdlfkja;l
+	
+@endsection
+
+@section('footer')
+	<!--script to something-->
+	<script src="/js/file.js"></script>
+@endsection
---
+++ views/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PostsController@index');
+
+
+//need a controller => PostsController
+
+//an Eloquent Model => Post
+
+// and an migration => create_posts_table
